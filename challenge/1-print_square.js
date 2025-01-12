51eaa5de78b6b8295fd8c8e9485d3ae45b371a1d
chutima puthachon <chutima.p8@gmail.com>
2025-01-13 09:23:09 +1100

    added task1

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
@@ -13,12 +13,12 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
+// use base 10(decimal) for parsing
+size = parseInt(process.argv[2], 10)
 
-size = parseInt(process.argv[2], 16)
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    process.stdout.write("#");
+  }
+  process.stdout.write("\n");
 }
